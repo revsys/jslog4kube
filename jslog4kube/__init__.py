ab71f5fa22eaa585159dc9ccc0af69826c9d068d
Stephen Spencer <stephen@revsys.com>
2017-07-17 04:03:37 -0500

    initialize the log registry for GMT

--- jslog4kube/__init__.py
+++ jslog4kube/__init__.py
@@ -1,9 +1,14 @@
 # pylint: disable=invalid-name, missing-docstring, too-few-public-methods
 
+import logging
+import time
+from pythonjsonlogger.jsonlogger import JsonFormatter
 from .bootstrap import LOG_ADDS, format_str
 from .kube.log_config import LOGGING
 from .kube.metadata_injector import KubeMetaInject
-from pythonjsonlogger.jsonlogger import JsonFormatter
+
+JsonFormatter.converter = time.gmtime
+
 
 try:
     from .gunicorn.dictconfig_logger import GunicornLogger
