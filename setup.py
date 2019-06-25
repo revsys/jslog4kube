d4c4c7c495117293378bf9784e66b26149f15e4b
Frank Wiles <frank@revsys.com>
2019-06-25 10:30:58 -0500

    Fix project README on PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import os
+
 from setuptools import setup
+
+BASE_DIR = os.path.dirname(__file__)
+
+with open(os.path.join(BASE_DIR, "README.md")) as f:
+    long_description = f.read()
 
 setup(
     name="jslog4kube",
     version="1.0.4",
     description="relatively hassle-free JSON logging for Kubernetes pods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="http://gitlab.com/stephen6/jslog4kube",
     author="Stephen D. Spencer",
     author_email="stephen@revsys.com",
